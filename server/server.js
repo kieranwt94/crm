3d4fa5471a9c03f4327dd0b526fc76daefbbb7bf
Kieran White <kieran@bluesquareoffices.com>
2020-09-07 20:30:43 +0100

    Adding middleware to routes.

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const logger = require('morgan');
-const bodyParser = require('body-parser');
-const path = require("path"); 
-const cors = require('cors'); 
-const rateLimit = require("express-rate-limit");
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
 
 const environment = process.env.NODE_ENV;
 const app = express();
 const port = process.env.PORT || 5000;
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, 
-    max: 100
+  windowMs: 15 * 60 * 1000,
+  max: 100,
 });
 
+const authRoutes = require('./routes/auth.routes');
 const brandRoutes = require('./routes/brand.routes');
 const customerRoutes = require('./routes/customer.routes');
 const dashboardRoutes = require('./routes/dashboard.routes');
@@ -21,35 +21,28 @@ const orderRoutes = require('./routes/order.routes');
 const serviceRoutes = require('./routes/service.routes');
 const userRoutes = require('./routes/user.routes');
 
+const authMiddlewares = require('./middlewares/auth.middleware');
+
 app.use(express.json());
 
 app.use(express.urlencoded({
-    extended: false
+  extended: false,
 }));
 
 app.use(limiter);
 
 if (environment !== 'production') {
-    app.use(logger('dev'));
-} 
+  app.use(logger('dev'));
+}
 
-const allowedOrigins = ['http://localhost:3000'];
-
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+  res.send('Hello World.');
 });
 
+app.use('/api/auth', authRoutes);
+app.use(authMiddlewares);
 app.use('/api/brands', brandRoutes);
 app.use('/api/customers', customerRoutes);
 app.use('/api/orders', orderRoutes);
@@ -58,5 +51,5 @@ app.use('/api/users', userRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 });
