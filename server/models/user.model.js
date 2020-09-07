e7968ece81883d8ba21313ec30c4031df9072efc
Kieran White <kieran@bluesquareoffices.com>
2020-09-07 20:29:15 +0100

    Sorting indentation

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,39 +1,39 @@
 const bcrypt = require('bcrypt-nodejs');
 
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        name: DataTypes.STRING,
-        password: DataTypes.STRING,
-        email_address: DataTypes.STRING
-    }, {});
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email_address: DataTypes.STRING
+  }, {});
 
-    function cryptPassword(password) {
-        return new Promise(function (resolve, reject) {
-            bcrypt.genSalt(10, function (err, salt) {
-                if (err) return reject(err);
-                bcrypt.hash(password, salt, function (err, hash) {
-                    if (err) return reject(err);
-                    return resolve(hash);
-                });
-            });
+  function cryptPassword(password) {
+    return new Promise(function (resolve, reject) {
+      bcrypt.genSalt(10, function (err, salt) {
+        if (err) return reject(err);
+        bcrypt.hash(password, salt, null, function (err, hash) {
+          if (err) return reject(err);
+          return resolve(hash);
         });
-    }
-
-    User.beforeCreate(function (user, options) {
-        return cryptPassword(user.password)
-            .then((success) => {
-                user.password = success;
-            })
-            .catch((err) => console.log(err));
+      });
     });
+  }
 
-    User.beforeBulkCreate(function (user, options) {
-        return cryptPassword(user.password)
-            .then((success) => {
-                user.password = success;
-            })
-            .catch((err) => console.log(err));
-    });
-    
-    return User;
+  User.beforeCreate(function (user, options) {
+    return cryptPassword(user.password)
+      .then((success) => {
+        user.password = success;
+      })
+      .catch((err) => console.log(err));
+  });
+
+  User.beforeBulkCreate(function (user, options) {
+    return cryptPassword(user.password)
+      .then((success) => {
+        user.password = success;
+      })
+      .catch((err) => console.log(err));
+  });
+
+  return User;
 };
